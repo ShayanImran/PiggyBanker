10eb00f986e53dfcc1b55f142457de950157fee0
nullzo <bensimms@knights.ucf.edu>
2019-03-02 20:57:24 -0500

    added purchase

--- app/src/main/java/com/example/piggybanker/MainActivity.java
+++ app/src/main/java/com/example/piggybanker/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.piggybanker;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,6 +9,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -49,4 +51,11 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    public void logPurchase(View view){
+       //when called, calls log activity
+        Intent intent = new Intent(MainActivity.this, logPurchase.class);
+        startActivity(intent);
+    }
+
 }
