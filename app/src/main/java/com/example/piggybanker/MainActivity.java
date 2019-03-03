e05e6654a03304bb1b35c860cf1e24feb1d823eb
Shayan Imran <shayanimran99@gmail.com>
2019-03-03 00:06:03 -0500

    test

--- app/src/main/java/com/example/piggybanker/MainActivity.java
+++ app/src/main/java/com/example/piggybanker/MainActivity.java
@@ -20,22 +20,10 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -57,5 +45,6 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(MainActivity.this, logPurchase.class);
         startActivity(intent);
     }
+
 
 }
